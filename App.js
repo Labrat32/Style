a099656e2945af2a7a7817e3ac6a5dd2a642d678
Ricu Ruotsinsalo <thericu@gmail.com>
2022-08-26 13:06:57 +0300

    stylish

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={[
+        styles.container, {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        }]}>
+        <View style={styles.Yks}>
+          <ImageBackground source={require('./assets/Nice_boat.jpg')}
+                           style={styles.tausta}
+          imageStyle={styles.boatLogo}/>
+          <Text style={styles.slogan}>Tosi siisti slogan</Text>
+        </View>
+        <View style={styles.Kaks}>
+          <Image style={styles.kuva} source={require('./assets/DK_OK.png')}/>
+          <Text>Kaksi</Text>
+        </View>
+        <View style={styles.Kol}>
+          <View style={styles.vasen}>
+            <Text>Vasen</Text>
+          </View>
+          <View style={styles.oikee}>
+            <Text>Oikee</Text>
+          </View>
+        </View>
+        <View style={styles.Nee}>
+          <Text>Footer</Text>
+        </View>
+      </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'gray',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'space-evenly',
+  },
+  boatLogo: {
+    borderBottomEndRadius: 35
+  },
+  tausta: {
+    resizeMode: 'cover',
+    height: "100%",
+    width: "100%"
+  },
+  Yks: {
+    flex: 1,
+    //height: 50,
+    //width: 50,
+    backgroundColor: 'red',
+    position: 'relative',
+  },
+  Kaks: {
+    //flex: 1,
+    height: 200,
+    width: 200,
+    alignSelf: 'center',
+    backgroundColor: 'yellow',
+    overflow: 'hidden',
+    borderWidth: 5,
+    borderBottomEndRadius: 20,
+  },
+  Kol: {
+    flex: 3,
+    flexDirection: 'row',
+    //height: 50,
+    //width: 50,
+    overflow: 'hidden',
+    backgroundColor: 'green',
+  },
+  Nee: {
+    height: 50,
+  },
+  vasen: {
+    backgroundColor: 'blue',
+    flex: 1,
+  },
+  oikee: {
+    backgroundColor: 'teal',
+    flex: 1,
+  },
+  kuva: {
+    resizeMode: 'stretch',
+    height: 200,
+    width: 200,
+    borderBottomEndRadius: 20,
+  },
+  slogan: {
+    backgroundColor: 'white',
+    width: 106,
+    position: 'absolute',
+    bottom: 30,
+    left: 30,
   },
 });
+
+export default App;
