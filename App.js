c9964591008b6d738e518e65c725409aa90fd77c
Ricu Ruotsinsalo <thericu@gmail.com>
2022-08-26 14:12:34 +0300

    Finish

--- App.js
+++ App.js
@@ -1,70 +1,176 @@
 import {StatusBar} from 'expo-status-bar';
-import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native';
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text, TouchableOpacity,
+  View,
+} from 'react-native';
+import PropsTypes from 'prop-types';
 import React from 'react';
+
+
+const ListItem = (props) => {
+  return (
+      <TouchableOpacity style={styles.container}>
+        <View style={styles.row}>
+          <Image
+              style={styles.image}
+              source={{uri: props.singleMedia.thumbnails.w160}}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.title}>{props.singleMedia.title}</Text>
+          <Text style={styles.text}>{props.singleMedia.description}</Text>
+        </View>
+      </TouchableOpacity>
+  );
+};
+
+ListItem.propTypes = {
+  singleMedia: PropsTypes.object,
+};
+// const App = () => {
+//   return (
+//       <View style={[
+//         styles.container, {
+//           // Try setting `flexDirection` to `"row"`.
+//           flexDirection: 'column',
+//         }]}>
+//         <View style={styles.Yks}>
+//           <ImageBackground source={require('./assets/Nice_boat.jpg')}
+//                            style={styles.tausta}
+//           imageStyle={styles.boatLogo}/>
+//           <Text style={styles.slogan}>Tosi siisti slogan</Text>
+//         </View>
+//         <View style={styles.Kaks}>
+//           <Image style={styles.kuva} source={require('./assets/DK_OK.png')}/>
+//           <Text>Kaksi</Text>
+//         </View>
+//         <View style={styles.Kol}>
+//           <View style={styles.vasen}>
+//             <Text>Vasen</Text>
+//           </View>
+//           <View style={styles.oikee}>
+//             <Text>Oikee</Text>
+//           </View>
+//         </View>
+//         <View style={styles.Nee}>
+//           <Text>Footer</Text>
+//         </View>
+//       </View>
+//   );
+// };
+
+const mediaArray = [
+  {
+    key: '0',
+    title: 'Title 1',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/161',
+    },
+    filename: 'http://placekitten.com/2048/1920',
+  },
+  {
+    key: '1',
+    title: 'Title 2',
+    description:
+        'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/164',
+    },
+    filename: 'http://placekitten.com/2041/1922',
+  },
+  {
+    key: '2',
+    title: 'Title 3',
+    description:
+        'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/167',
+    },
+    filename: 'http://placekitten.com/2039/1920',
+  },
+];
 
 const App = () => {
   return (
-      <View style={[
-        styles.container, {
-          // Try setting `flexDirection` to `"row"`.
-          flexDirection: 'column',
-        }]}>
+      <View style={styles.page}>
         <View style={styles.Yks}>
           <ImageBackground source={require('./assets/Nice_boat.jpg')}
                            style={styles.tausta}
-          imageStyle={styles.boatLogo}/>
-          <Text style={styles.slogan}>Tosi siisti slogan</Text>
+                           imageStyle={styles.boatLogo}/>
+          <Text style={styles.slogan}>Laivakissat</Text>
         </View>
         <View style={styles.Kaks}>
-          <Image style={styles.kuva} source={require('./assets/DK_OK.png')}/>
-          <Text>Kaksi</Text>
-        </View>
-        <View style={styles.Kol}>
-          <View style={styles.vasen}>
-            <Text>Vasen</Text>
-          </View>
-          <View style={styles.oikee}>
-            <Text>Oikee</Text>
-          </View>
-        </View>
-        <View style={styles.Nee}>
-          <Text>Footer</Text>
+      <FlatList
+          data={mediaArray}
+          renderItem={({item}) => <ListItem singleMedia={item}/>}
+      />
         </View>
       </View>
   );
 };
 
 const styles = StyleSheet.create({
+  page: {
+    flex: 1
+  },
   container: {
-    backgroundColor: 'gray',
     flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'space-evenly',
+    backgroundColor: '#2b2b2b',
+    flexDirection: 'row',
+    paddingBottom: 5,
+  },
+  text: {
+    fontStyle: 'italic',
+    color: "#869299"
+  },
+  image: {
+    flex: 1,
+    borderRadius: 2,
+    borderBottomLeftRadius: 40,
+    borderTopEndRadius: 40,
+    resizeMode: 'stretch',
+  },
+  row: {
+    flex: 1,
+    backgroundColor: '#3c3f41',
+    padding: 10,
+    fontStyle: 'italic'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: "#bababa"
   },
   boatLogo: {
-    borderBottomEndRadius: 35
+    borderBottomEndRadius: 35,
   },
   tausta: {
     resizeMode: 'cover',
-    height: "100%",
-    width: "100%"
+    height: '100%',
+    width: '100%',
   },
   Yks: {
     flex: 1,
     //height: 50,
     //width: 50,
-    backgroundColor: 'red',
+    backgroundColor:'black',
     position: 'relative',
   },
   Kaks: {
-    //flex: 1,
-    height: 200,
-    width: 200,
-    alignSelf: 'center',
-    backgroundColor: 'yellow',
-    overflow: 'hidden',
-    borderWidth: 5,
-    borderBottomEndRadius: 20,
+    flex: 3,
+    // height: 200,
+    // width: 200,
+    // alignSelf: 'center',
+    // backgroundColor: 'yellow',
+    // overflow: 'hidden',
+    // borderWidth: 5,
+    // borderBottomEndRadius: 20,
   },
   Kol: {
     flex: 3,
@@ -92,11 +198,15 @@ const styles = StyleSheet.create({
     borderBottomEndRadius: 20,
   },
   slogan: {
-    backgroundColor: 'white',
-    width: 106,
+    backgroundColor: 'rgba(0,0,0,0.36)',
+    width: 150,
     position: 'absolute',
-    bottom: 30,
-    left: 30,
+    padding: 3,
+    top: 80,
+    left: 23,
+    color: "white",
+    fontSize: 30,
+
   },
 });
 
